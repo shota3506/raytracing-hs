3e8d5d98158b93f6a27a5ead2ab401c2ce0953e2
shota3506 <s.shota.710.3506@gmail.com>
2026-02-22 17:53:45 +0900

    output progress log

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,24 @@
 module Main (main) where
 
+import Control.Monad (forM_)
+import System.IO (hPutStrLn, stderr)
+
 main :: IO ()
 main = do
   let imageWidth :: Int = 256
       imageHeight :: Int = 256
 
   putStrLn ("P3\n" ++ show imageWidth ++ " " ++ show imageHeight ++ "\n255")
-  mapM_
-    putStrLn
-    [ show ir ++ " " ++ show ig ++ " " ++ show ib
-    | j <- [0 .. imageHeight - 1],
-      i <- [0 .. imageWidth - 1],
+
+  forM_ [0 .. imageHeight - 1] $ \j -> do
+    hPutStrLn stderr ("Scanlines remaining: " ++ show (imageHeight - j))
+    forM_ [0 .. imageWidth - 1] $ \i -> do
       let r :: Double = fromIntegral i / fromIntegral (imageWidth - 1)
           g :: Double = fromIntegral j / fromIntegral (imageHeight - 1)
           b :: Double = 0.0
           ir = round (255.999 * r) :: Int
           ig = round (255.999 * g) :: Int
           ib = round (255.999 * b) :: Int
-    ]
+      putStrLn $ show ir ++ " " ++ show ig ++ " " ++ show ib
+
+  hPutStrLn stderr "Done."

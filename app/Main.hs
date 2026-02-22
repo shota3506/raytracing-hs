7eb7a69ee33b90b1e852f63dd656a7108891e97d
shota3506 <s.shota.710.3506@gmail.com>
2026-02-22 22:28:04 +0900

    colorize sphere by normal vectors

--- app/Main.hs
+++ app/Main.hs
@@ -6,23 +6,29 @@ import Ray
 import System.IO (hPutStrLn, stderr)
 import Vec3
 
-hitSphere :: Vec3 -> Double -> Ray -> Bool
+hitSphere :: Vec3 -> Double -> Ray -> Double
 hitSphere center radius r =
   let oc = vSub center (rayOrigin r)
       a = dot (rayDirection r) (rayDirection r)
       b = -(2.0 * dot oc (rayDirection r))
       c = dot oc oc - radius * radius
       discriminant = b * b - 4 * a * c
-   in discriminant >= 0
+   in if discriminant < 0 then -1.0 else (-b - sqrt discriminant) / (2.0 * a)
 
 rayColor :: Ray -> Color
 rayColor r
-  | hitSphere (Vec3 0 0 (-1)) 0.5 r = Vec3 1 0 0
+  | t > 0 =
+      let n = vUnit (rayAt r t `vSub` sphereCenter)
+          Vec3 x y z = n
+       in vScale 0.5 (Vec3 (x + 1) (y + 1) (z + 1))
   | otherwise =
       let unitDirection = vUnit (rayDirection r)
           (Vec3 _ y _) = unitDirection
           a = 0.5 * (y + 1.0)
        in vScale (1.0 - a) (Vec3 1.0 1.0 1.0) `vAdd` vScale a (Vec3 0.5 0.7 1.0)
+  where
+    sphereCenter = Vec3 0 0 (-1)
+    t = hitSphere sphereCenter 0.5 r
 
 main :: IO ()
 main = do

2a7ce985ee992ec16448b1390ce02e883d712571
shota3506 <s.shota.710.3506@gmail.com>
2026-02-22 18:25:09 +0900

    implement vector operators

--- src/Vec3.hs
+++ src/Vec3.hs
@@ -8,3 +8,28 @@ vAdd (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 + x2) (y1 + y2) (z1 + z2)
 
 vSub :: Vec3 -> Vec3 -> Vec3
 vSub (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 - x2) (y1 - y2) (z1 - z2)
+
+vMul :: Vec3 -> Vec3 -> Vec3
+vMul (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 * x2) (y1 * y2) (z1 * z2)
+
+vScale :: Double -> Vec3 -> Vec3
+vScale s (Vec3 x y z) = Vec3 (x * s) (y * s) (z * s)
+
+vDiv :: Double -> Vec3 -> Vec3
+vDiv s = vScale (1 / s)
+
+dot :: Vec3 -> Vec3 -> Double
+dot (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
+
+cross :: Vec3 -> Vec3 -> Vec3
+cross (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) =
+  Vec3 (y1 * z2 - z1 * y2) (z1 * x2 - x1 * z2) (x1 * y2 - y1 * x2)
+
+vLength :: Vec3 -> Double
+vLength v = sqrt (dot v v)
+
+vUnit :: Vec3 -> Vec3
+vUnit v = vDiv (vLength v) v
+
+vNegate :: Vec3 -> Vec3
+vNegate = vScale (-1)
--- test/Spec.hs
+++ test/Spec.hs
@@ -18,3 +18,31 @@ main = hspec $ do
 
     it "subtracts with zero vector" $ do
       vSub (Vec3 1 2 3) (Vec3 0 0 0) `shouldBe` Vec3 1 2 3
+
+  describe "vMul" $ do
+    it "multiplies two vectors element-wise" $ do
+      vMul (Vec3 2 3 4) (Vec3 5 6 7) `shouldBe` Vec3 10 18 28
+
+  describe "vScale" $ do
+    it "scales a vector" $ do
+      vScale 2 (Vec3 1 2 3) `shouldBe` Vec3 2 4 6
+
+  describe "vDiv" $ do
+    it "divides a vector by scalar" $ do
+      vDiv 2 (Vec3 2 4 6) `shouldBe` Vec3 1 2 3
+
+  describe "dot" $ do
+    it "computes dot product" $ do
+      dot (Vec3 1 2 3) (Vec3 4 5 6) `shouldBe` 32
+
+  describe "cross" $ do
+    it "computes cross product" $ do
+      cross (Vec3 1 0 0) (Vec3 0 1 0) `shouldBe` Vec3 0 0 1
+
+  describe "vLength" $ do
+    it "computes length" $ do
+      vLength (Vec3 3 4 0) `shouldBe` 5
+
+  describe "vNegate" $ do
+    it "negates a vector" $ do
+      vNegate (Vec3 1 (-2) 3) `shouldBe` Vec3 (-1) 2 (-3)

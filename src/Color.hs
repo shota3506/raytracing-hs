06cc7b52a16077efc429d9f655585ce97eae8268
shota3506 <s.shota.710.3506@gmail.com>
2026-02-24 07:54:59 +0900

    apply gamma correction

--- src/Color.hs
+++ src/Color.hs
@@ -5,11 +5,16 @@ import Vec3
 
 type Color = Vec3
 
+linearToGamma :: Double -> Double
+linearToGamma x
+  | x > 0 = sqrt x
+  | otherwise = 0
+
 formatColor :: Color -> String
 formatColor (Vec3 r g b) =
   show rbyte ++ " " ++ show gbyte ++ " " ++ show bbyte
   where
     intensity = Interval 0.0 0.999
-    rbyte = floor (255.999 * clamp intensity r) :: Int
-    gbyte = floor (255.999 * clamp intensity g) :: Int
-    bbyte = floor (255.999 * clamp intensity b) :: Int
+    rbyte = floor (255.999 * clamp intensity (linearToGamma r)) :: Int
+    gbyte = floor (255.999 * clamp intensity (linearToGamma g)) :: Int
+    bbyte = floor (255.999 * clamp intensity (linearToGamma b)) :: Int
--- test/ColorSpec.hs
+++ test/ColorSpec.hs
@@ -5,12 +5,23 @@ import Test.Hspec
 import Vec3 (Vec3 (..))
 
 spec :: Spec
-spec = describe "formatColor" $ do
-  it "formats normal color" $
-    formatColor (Vec3 1 0 0.5) `shouldBe` "255 0 127"
+spec = do
+  describe "linearToGamma" $ do
+    it "returns sqrt for positive values" $
+      linearToGamma 0.25 `shouldBe` 0.5
 
-  it "clamps values over 1" $
-    formatColor (Vec3 1.5 0 0) `shouldBe` "255 0 0"
+    it "returns 0 for zero" $
+      linearToGamma 0 `shouldBe` 0
 
-  it "clamps negative values" $
-    formatColor (Vec3 (-0.5) 0 0) `shouldBe` "0 0 0"
+    it "returns 0 for negative values" $
+      linearToGamma (-1.0) `shouldBe` 0
+
+  describe "formatColor" $ do
+    it "formats normal color with gamma correction" $
+      formatColor (Vec3 1 0 0.5) `shouldBe` "255 0 181"
+
+    it "clamps values over 1" $
+      formatColor (Vec3 1.5 0 0) `shouldBe` "255 0 0"
+
+    it "clamps negative values" $
+      formatColor (Vec3 (-0.5) 0 0) `shouldBe` "0 0 0"
